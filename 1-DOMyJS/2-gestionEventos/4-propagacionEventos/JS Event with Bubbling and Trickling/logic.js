9264fa659c9bf9832c3beb0a6e0096db9469dbbe
Setillo <setillofm@gmail.com>
2022-03-19 13:28:24 +0100

    refactor

--- 1-DOMyJS/2-gestionEventos/4-propagacionEventos/JS Event with Bubbling and Trickling/logic.js
+++ 1-DOMyJS/2-gestionEventos/4-propagacionEventos/JS Event with Bubbling and Trickling/logic.js
@@ -1,7 +1,7 @@
-function onLoad(){
-  for(let div of document.getElementsByTagName("div")) {
-    //div.addEventListener("click", alertBubblingFromListener);
-    //div.addEventListener("click", alertTricklingFromListener, true);
+function onLoad() {
+  for (let div of document.getElementsByTagName("div")) {
+    // div.addEventListener("click", alertBubblingFromListener);
+    // div.addEventListener("click", alertTricklingFromListener, true);
     div.addEventListener("click"
       , alertBubblingFromListenerWithoutPropagation);
     div.addEventListener("click"
@@ -9,7 +9,7 @@ function onLoad(){
   }
 }
 
-function getMsg(event, element){
+function getMsg(event, element) {
   if (event.count == undefined) {
     event.count = 0;
   }
@@ -17,21 +17,21 @@ function getMsg(event, element){
   return element.getAttribute("class") + " con " + event.count;
 }
 
-function alertBubblingFromHTML(event, element){
+function alertBubblingFromHTML(event, element) {
   console.log("Bubbling form HTML: " + getMsg(event, element));
 }
 
-function alertBubblingFromListener(event){
+function alertBubblingFromListener(event) {
   console.log("Bubbling form addEventListener: " + getMsg(event, this));
 }
 
-function alertTricklingFromListener(event){
-  console.log("Trickling form addEventListener: " 
+function alertTricklingFromListener(event) {
+  console.log("Trickling form addEventListener: "
     + getMsg(event, this));
 }
 
-function alertBubblingFromListenerWithoutPropagation(event){
-  console.log("Bubbling form addEventListener without Propagation: " 
+function alertBubblingFromListenerWithoutPropagation(event) {
+  console.log("Bubbling form addEventListener without Propagation: "
     + getMsg(event, this));
   if (this.getAttribute("class") === "nivel-2 derecha") {
     console.log("PARA burbujeo");
@@ -39,8 +39,8 @@ function alertBubblingFromListenerWithoutPropagation(event){
   }
 }
 
-function alertTricklingFromListenerWithoutPropagation(event){
-  console.log("Trickling form addEventListener without Propagation: " 
+function alertTricklingFromListenerWithoutPropagation(event) {
+  console.log("Trickling form addEventListener without Propagation: "
     + getMsg(event, this));
   if (this.getAttribute("class") === "nivel-3 izquierda") {
     console.log("PARA goteo");
